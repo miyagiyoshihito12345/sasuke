a8256116c6f98506e6ff58c92458a60a88ed5c21
yoshihitomiyagi <miyagiyoshihito12345@gmail.com>
2023-12-28 21:20:47 +0900

    Add: 実装完了

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -4,5 +4,14 @@ class StaticpagesController < ApplicationController
 
   def result
     @number = rand(1..4)
+    @name = if @number == 1
+              "神威"
+            elsif @number == 2
+              "天照・加具土命"
+            elsif @number == 3
+              "別天神"
+            elsif @number == 4
+              "天照・月読"
+            end
   end
 end
